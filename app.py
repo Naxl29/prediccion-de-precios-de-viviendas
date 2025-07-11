4c1e68a622d75563b6b773cfb9b9b3e11b3b9848
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-07-10 21:09:53 -0500

    Resumen estadistico

--- app.py
+++ app.py
@@ -10,5 +10,32 @@ def mostrar_tabla():
     datos_html = datos.to_html(classes='table table-bordered', index=False)
     return render_template('tabla.html', tabla=datos_html)
 
+@app.route('/resumen')
+def resumen_estadistico():
+    ruta = r"C:\laragon\www\PYTHON\prediccion-de-precios-de-viviendas\dataset\dataset_viviendas.xlsx"
+    datos = pd.read_excel(ruta,  header=1, engine='openpyxl') 
+
+    total_viviendas = len(datos)
+
+    datos['precio_m2'] = datos['precio'] / datos['area']
+    promedio_m2 = datos['precio_m2'].mean()
+
+    def claficar_tipo(desc):
+        if isinstance(desc, str):
+            if 'casa' in desc.lower():
+                return 'Casa'
+            elif 'apartamento' in desc.lower():
+                return 'Apartamento'
+        return 'Otro'
+
+    datos['tipo_vivienda'] = datos['descripcion'].apply(claficar_tipo)
+    conteo_tipo = datos['tipo_vivienda'].value_counts()
+
+    return render_template('resumen.html',
+                           total=total_viviendas,
+                           promedio_m2=round(promedio_m2, 2),
+                            conteo=conteo_tipo.to_dict()
+                            )
+
 if __name__ == '__main__':
     app.run(debug=True)
