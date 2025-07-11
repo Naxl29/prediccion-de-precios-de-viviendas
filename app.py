160381cc2ac71242f03a46662d533fe4c4c773c5
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-07-11 10:15:16 -0500

    Arreglos en el dashboard y en el app

--- app.py
+++ app.py
@@ -1,12 +1,55 @@
 from flask import Flask, render_template
 import pandas as pd
 from Controllers.grafico_dispersion import generar_dispersion
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def mostrar_dashboard():
-    return render_template('dashboard.html')
+def dashboard():
+    try:
+        ruta = r"C:\laragon\www\PYTHON\prediccion-de-precios-de-viviendas\dataset\dataset_viviendas.xlsx"
+
+        if os.path.exists(ruta):
+            datos = pd.read_excel(ruta, header=1, engine='openpyxl')
+            total_viviendas = len(datos)
+            precio_promedio = datos['precio'].mean()
+            area_promedio = datos['area'].mean()
+            datos['precio_m2'] = datos['precio'] / datos['area']
+            precio_m2_promedio = datos['precio_m2'].mean()
+
+            def clasificar_tipo(desc):
+                if isinstance(desc, str):
+                    if 'casa' in desc.lower():
+                        return 'Casa'
+                    elif 'apartamento' in desc.lower():
+                        return 'Apartamento'
+                return 'Otro'
+            
+            datos['tipo_vivienda'] = datos['descripcion'].apply(clasificar_tipo)
+            conteo_tipo = datos['tipo_vivienda'].value_counts()
+
+            estadisticas = {
+                'total_viviendas': total_viviendas,
+                'precio_promedio': f"{precio_promedio:,.0f}",
+                'area_promedio': f"{area_promedio:,.0f}",
+                'precio_m2_promedio': f"{precio_m2_promedio:,.0f}",
+                'casas_count': conteo_tipo.get('Casa', 0),
+                'apartamentos_count': conteo_tipo.get('Apartamento', 0),
+                'otros_count': conteo_tipo.get('Otro', 0),
+                'precio_min':f"{datos['precio'].min():,.0f}",
+                'precio_max': f"{datos['precio'].max():,.0f}",
+                'area_comum': f"{datos['area'].mean():,.0f}"
+            }
+
+            print("Estadisticas calculadas correctamente.")
+            return render_template('dashboard.html', **estadisticas)
+        else:
+            print("El archivo no existe en la ruta especificada.")
+            return render_template('dashboard.html')
+    except Exception as e:
+        print(f"Error en el dashboard: {e}")
+        return render_template('dashboard.html')
 
 @app.route('/tabla')
 def mostrar_tabla():
